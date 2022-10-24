7744bbfc530fe3993aa535a54b365b8fe62b8e59
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2022-10-24 12:22:24 +0200

    initial upload - incomplete

---
+++ commerce-react-app/src/components/main page/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav>
+        <img src="" alt="" className="logo" />
+        <ul>
+            <li><a href="../economy/Economy.jsx">Econoy</a></li>
+            {/* <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li> */}
+        </ul>
+    </nav>
+  )
+}
+
+export default Nav
---
+++ commerce-react-app/src/components/principles of financial accounting/PrinciplesOfFinancialAccounting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PrinciplesOfManagementalAccounting() {
+  return (
+    <div>PrinciplesOfFinancialAccounting</div>
+  )
+}
+
+export default PrinciplesOfManagementalAccounting
